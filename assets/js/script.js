82486be5a28a833fe1e02d55b5c853fac4f46d4a
Pratik Majumdar <majumderpratik15@gmail.com>
2021-02-05 22:46:22 +0530

    final editing of mod2 done
    now will branch to udemy-spa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
-var reload = document.querySelector("#reload");
-reload.addEventListener("click", myFunction);
+const reload = document.querySelector("#reload");
+const contactButton = document.querySelector("#scrolldown");
+const scrollToTop = document.querySelector("#btnScrollToTop");
 
-function myFunction() {	
+scrollToTop.addEventListener("click", scrollUp);
+
+reload.addEventListener("click", reloadMe);
+
+contactButton.addEventListener("click", scrollDown)
+
+
+function scrollDown() {
+
+    window.scrollTo({
+        top:document.body.scrollHeight,
+        left:0,
+        behavior:"smooth"
+    })
+}
+
+function scrollUp() {
+
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
+    })
+}
+
+function reloadMe() {	
     window.location.reload()			
 }
 
